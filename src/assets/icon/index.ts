ee144f440b022e55052f666a6c061101e325c217
SEOKKAMONI <sj01048260279@gmail.com>
2023-08-09 14:57:00 +0900

    ADD :: Icon

---
+++ src/assets/icon/index.ts
@@ -0,0 +1,6 @@
+export { default as IconAdd } from "./IconAdd";
+export { default as IconArrowBottom } from "./IconArrowBottom";
+export { default as IconArrowLeft } from "./IconArrowLeft";
+export { default as IconArrowRight } from "./IconArrowRight";
+export { default as IconArrowTop } from "./IconArrowTop";
+export { default as IconClose } from "./IconClose";
