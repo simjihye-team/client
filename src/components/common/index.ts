f3e664cc7fb9c6491d2a1485616c9b468e66d8f7
SEOKKAMONI <sj01048260279@gmail.com>
2023-08-09 13:16:27 +0900

    ADD :: Flex

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,5 @@
 export { default as Button } from "./Button/Button";
 export { default as Input } from "./Input/Input";
 export { default as Text } from "./Text/Text";
+export { default as Column } from "./Flex/Column";
+export { default as Row } from "./Flex/Row";
